597f64f3ab9aa2b45e174f37ed2d4fff12f85ad8
Sovre <jorgensovre@jrgens-mbp.uia.no>
2017-01-31 13:09:39 +0100

    En av oppgavene

--- hello.go
+++ hello.go
@@ -1,8 +1,18 @@
 package main
 
 import "fmt"
+import "math"
 
 func main() {
 	fmt.Println("Hello, 世界")
+<<<<<<< HEAD
+	fmt.Println("Jonas")
+	log2()
+=======
 	fmt.Println("Søvre var her")
+>>>>>>> 7d6923075edfbdeba0690b5054f372edb5962218
+	fmt.Println("Dette er en test. Kos og klemz, Vegard.")
+}
+func log2() {
+	fmt.Println(math.Log2(8))
 }
