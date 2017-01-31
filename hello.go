beeb251e9771faad29c90a7cdcb1d461d5f3a68d
christoffer <christoffer>
2017-01-31 13:15:33 +0100

    ny metode

--- hello.go
+++ hello.go
@@ -3,15 +3,17 @@ package main
 import "fmt"
 import "math"
 
+func printChris() {
+	fmt.Println("Christoffer sier hei")
+}
+
 func main() {
 	fmt.Println("Hello, 世界")
-<<<<<<< HEAD
 	fmt.Println("Jonas")
 	log2()
-=======
 	fmt.Println("Søvre var her")
->>>>>>> 7d6923075edfbdeba0690b5054f372edb5962218
 	fmt.Println("Dette er en test. Kos og klemz, Vegard.")
+	printChris()
 }
 func log2() {
 	fmt.Println(math.Log2(8))
