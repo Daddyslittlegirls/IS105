95e5cdc8fefafbd11d39afcb2c7147bc25658f0c
Elias Burkan <Elias@eliass-mbp.uia.no>
2017-01-31 13:16:09 +0100

    elias test

--- hello.go
+++ hello.go
@@ -19,9 +19,15 @@ func main() {
 	fmt.Println("Søvre var her")
 >>>>>>> 7d6923075edfbdeba0690b5054f372edb5962218
 	fmt.Println("Dette er en test. Kos og klemz, Vegard.")
+<<<<<<< HEAD
+}
+func log2() {
+	fmt.Println(math.Log2(8))
+=======
 	fmt.Println("Mosvold Salvesen")
 }
 func log2() {
 	fmt.Println(math.Log2(8))
 >>>>>>> 7998197b64daf3ac8bac1172b8c9ccc8449ae757
+>>>>>>> d209a45646246176a4b38c5cdaca67e607cd63c8
 }
