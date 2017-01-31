55e7ef8785480d00fa62a2f90c20ad7b2d246ec6
christoffer <christoffer>
2017-01-31 14:42:29 +0100

    Lagt til log.go og endret hello.go for å tilpasse oppgave 1.2.2.4

--- hello.go
+++ hello.go
@@ -1,20 +1,20 @@
 package main
 
 import "fmt"
-import "math"
-
-func printChris() {
-	fmt.Println("Christoffer sier hei")
-}
+import "os"
 
 func main() {
+
 	fmt.Println("Hello, 世界")
+	fmt.Println("Elias var her")
+	fmt.Println("Hallo, Jørgen MS var her")
 	fmt.Println("Jonas")
-	log2()
 	fmt.Println("Søvre var her")
 	fmt.Println("Dette er en test. Kos og klemz, Vegard.")
-	printChris()
+	fmt.Println("Christoffer sier hei")
+	fmt.Println("Mosvold Salvesen")
+	fmt.Println("Andrea gjør en endring")
+	log2(8.0)
 }
-func log2() {
-	fmt.Println(math.Log2(8))
-}
+
+
