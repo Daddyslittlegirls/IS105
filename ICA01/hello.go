0fce6f05890b61230fb83d63574a9d766ffde59c
Jonas Omdal <jonaso15@student.uia.no>
2017-05-15 13:51:12 +0200

    Jonas, La til en endring i Hello.go fila

--- ICA01/hello.go
+++ ICA01/hello.go
@@ -1,4 +1,4 @@
-package main
+﻿package main
 
 import "fmt"
 import "os"
@@ -8,7 +8,7 @@ func main() {
 	fmt.Println("Hello, 世界")
 	fmt.Println("Elias var her XDD ! SISTE COMMIT EVER LOL")
 	fmt.Println("Hallo, Jørgen MS var her")
-	fmt.Println("Jonas")
+	fmt.Println("Hallo, Jonas")
 	fmt.Println("Søvre var her")
 	fmt.Println("Dette er en test. Kos og klemz, Vegard.")
 	fmt.Println("Christoffer sier hei")
