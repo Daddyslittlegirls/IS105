afb77a824ccc95dbe101e275fbfb8841476937a7
Sovre <jorgensovre1997@gmail.com>
2017-05-16 13:51:42 +0200

    Gjorde en endring i hello.go

--- ICA01/hello.go
+++ ICA01/hello.go
@@ -10,6 +10,7 @@ func main() {
 	fmt.Println("Hallo, Jørgen MS var her")
 	fmt.Println("Hallo, Jonas her")
 	fmt.Println("Søvre var her")
+	fmt.Println("Søvre var her på nytt");
 	fmt.Println("Dette er en test. Kos og klemz, Vegard!")
 	fmt.Println("Christoffer sa hallo")
 	fmt.Println("Mosvold Salvesen")
